8fcc3dd0335a49c02dc546c27801e35eb9c302f2
CormacDC <60583567+CormacDC@users.noreply.github.com>
2022-02-07 17:10:47 -0500

    typo fix

--- Day3/DayThreePt2.java
+++ Day3/DayThreePt2.java
@@ -4,7 +4,7 @@ import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.stream.Stream;
 
-// very unefficient, > n^2 time complexity
+// very inefficient, > n^2 time complexity
 
 public class DayThreePt2 {
 
